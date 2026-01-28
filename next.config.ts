696a11e3ff44775e5ac2775c8314774016862542
DanielLeemor <daniel.leemor@gmail.com>
2026-01-28 13:26:46 +0200

    Configure image domains for Next.js

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,26 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api.screenshotone.com',
+        pathname: '/take',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.google.com',
+        pathname: '/s2/favicons',
+      },
+      // Allow all domains for icons/screenshots flexibility since we aggregate content
+      // In strict production, this might be restricted, but for a directory of user content:
+      {
+        protocol: 'https',
+        hostname: '**',
+      }
+    ],
+  },
 };
 
 export default nextConfig;
